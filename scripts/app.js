1d662842dcccc311a7e2c9e87207b8d77220694a
lucasyule2212 <lyrma@cin.ufpe.br>
2021-02-19 17:17:54 -0300

    Update app.js
    Adicionando e Alterando Documentos pelo JS

--- scripts/app.js
+++ scripts/app.js
@@ -13,25 +13,90 @@
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
-  //Lendo Documentos de Collections =>
   let dataBase = firebase.firestore();
+  //Lendo Documentos de Collections =>
   dataBase.collection("turmaA").get().then((snapshot)=>{
         snapshot.forEach((doc) => {
-            let aluno = doc.data()
+            let aluno = doc.data();
             console.log(aluno);
         });
   });
 
   //Lendo o Documento a partir do ID do Documento =>
-  let docRef = dataBase.collection("turmaA").doc("yiksPqZ8HV1rI1Y1P0fs");
+  let docRef = dataBase.collection("turmaA").doc("Aluno4");
   docRef.get().then((doc)=>{
       console.log(doc.data());
   })
   
   //Lendo o Documento a partir de uma informaçao mais específica e usando comparadores como o "=="
-  dataBase.collection("turmaA").where("Nome","==","Yule").get().then(snapshot=>{
+  dataBase.collection("turmaA").where("Nome","==","Denya").get().then(snapshot=>{
     snapshot.forEach(doc => {
       let aluno = doc.data();
       console.log(aluno.Nome);
     });
+  })
+
+  //Adicionando e Alterando Documentos a partir do JS
+   const TURMA = "turmaA";
+  // dataBase.collection(TURMA).add({
+  //   Nome:"Denya",
+  //   Idade:"21",
+  //   Notas:{
+  //     ["Nota 1"] : 9.5,
+  //     ["Nota 2"] :  8.5
+  //   },
+  //   Advertencias:{
+
+  //   }
+  // }).then((doc)=>{
+  //   console.log("Documento inserido com sucesso!",doc);
+  // }).catch(err=>{
+  //   console.log(err);
+  // })
+
+  //Adicionando e definindo ID nao randomico 
+  //METODO "SET" NAO RETORNA NENHUM DOCUMENTO, O "ADD" E O "GET" RETORNA!
+  dataBase.collection(TURMA).doc("Aluno4").set({
+    Nome:"Luna",
+    Idade:"1.5",
+    Notas:{
+      ["Nota 1"] : 0,
+      ["Nota 2"] :  0
+    },
+    Advertencias:{
+
+    }
+  }).then(()=>{
+    console.log("Documento inserido com sucesso!");
+  }).catch(err=>{
+    console.log(err);
+  })
+
+  //Alterando Documento
+  //METODO "SET" E "UPDATE" CONSEGUEM ADICIONAR NOVOS CAMPOS
+  dataBase.collection(TURMA).doc("Aluno4").set({
+    Nome:"Luna",
+    Sobrenome:"Maria",
+    Idade:1.5,
+    Notas:{
+      ["Nota 1"] : 0,
+      ["Nota 2"] :  0
+    },
+    Advertencias:{
+
+    }
+  }).then(()=>{
+    console.log("Documento alterado com sucesso!");
+  }).catch(err=>{
+    console.log(err);
+  })
+
+  //Alterando somente o campo desejado do Documento
+  dataBase.collection(TURMA).doc("l1Mhccf8QCyfohtt2I9V").update(
+    {
+    Sobrenome: "Louisa"
+  }).then(()=>{
+    console.log("Documento alterado com sucesso!");
+  }).catch(err=>{
+    console.log(err);
   })
